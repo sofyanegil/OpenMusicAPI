a311beaf50cc15e7b372ec9f5df02fb5e352e671
Sofyan Egi <sofyanegil@gmail.com>
2022-07-20 10:01:51 +0700

    feat: handler and service for add album

---
+++ src/api/albums/handler.js
@@ -0,0 +1,49 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postAlbumHandler = this.postAlbumHandler.bind(this);
+  }
+
+  async postAlbumHandler(request, h) {
+    try {
+      await this._validator.validateAlbumPayload(request.payload);
+
+      const { name, year } = request.payload;
+
+      const albumId = await this._service.addAlbum({ name, year });
+
+      const response = h.response({
+        status: 'success',
+        data: {
+          albumId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Internal server error',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = AlbumsHandler;
