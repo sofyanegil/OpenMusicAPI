fa5007128c9e27b713852d49df342b9e0824bc98
Sofyan Egi <sofyanegil@gmail.com>
2022-07-20 14:56:07 +0700

    feat: handler and service for add songs

---
+++ src/api/songs/handler.js
@@ -0,0 +1,53 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postSongHandler = this.postSongHandler.bind(this);
+  }
+
+  async postSongHandler(request, h) {
+    try {
+      await this._validator.validateSongPayload(request.payload);
+
+      const {
+        title, year, performer, genre, duration, albumId,
+      } = request.payload;
+
+      const songId = await this._service.addSong({
+        title, year, performer, genre, duration, albumId,
+      });
+
+      const response = h.response({
+        status: 'success',
+        data: {
+          songId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Internal server error',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = SongsHandler;
