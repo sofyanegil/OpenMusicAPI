d8d70d7aa2cd1fcb6ee2ed6ad6956c4e9055c4d4
Sofyan Egi <sofyanegil@gmail.com>
2022-07-20 12:50:03 +0700

    feat: create songs table migrations

---
+++ migrations/1658295392801_create-table-songs.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(180)',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    performer: {
+      type: 'VARCHAR(180)',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(180)',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+    },
+    albumId: {
+      type: 'VARCHAR(50)',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
